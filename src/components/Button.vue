72eef68757bed73ded85d9607039eb10e9674820
abzhalimov-artem <abzalimov.artem.ed@gmail.com>
2025-11-17 18:26:41 +0300

    add style and component button

---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="button">
+    <div class="button__icon">
+      <slot name="icon" />
+      <slot name="text" />
+    </div>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  border: none;
+  border-radius: 16px;
+  background: var(--gradient);
+  padding: 14px;
+  font-family: var(--font), sans-serif;
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--color-primary);
+  cursor: pointer;
+}
+
+.button:hover {
+  background: var(--gradient-inverted);
+}
+
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <button class="button" type="button">
+    <slot/>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  padding: 15px;
+  outline: 2px solid blue;
+  border-radius: 14px;
+  transition: all 0.5s ease-in-out;
+  cursor: pointer;
+
+  color: #272E37;
+  background-color: antiquewhite;
+  border: none;
+  box-shadow: 8px 5px 5px gray;
+}
+
+.button:hover {
+transform: scale(1.1);
+}
+</style>
