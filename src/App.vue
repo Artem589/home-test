912fe5b7a5551cd5658c14cfd4d9162523bae063
abzhalimov-artem <abzalimov.artem.ed@gmail.com>
2025-12-10 14:01:29 +0300

    add cardcomponent

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,98 @@
 <script setup>
-import Button from "./components/Button.vue";
-import Stat from "./components/Stat.vue";
+import ScoreLikes from "./components/ScoreLikes.vue";
+import CardWord from "./components/CardWord.vue";
+import {ref} from "vue";
 
-import CitySelect from "./components/CitySelect.vue";
 
-const data = {
-  label: 'Влажность',
-  stat: '90%'
-}
+const isTurn = ref(false)
 
-const getCity = (city) => {
-  console.log(city)
+const cards = ref([
+  {
+    isTurn: false,
+    ruText: 'непризнанный',
+    enText: 'unadmitted',
+  },
+  {
+    isTurn: false,
+    ruText: 'тычинка',
+    enText: 'stamen',
+  }
+])
+
+const turnCard = (card) => {
+  card.isTurn = true
+  console.log(card)
 }
 </script>
 
 <template>
-  <main class="main">
-<!--    <Stat v-bind="data"/>-->
-<!--    <Stat stat="0 %"/>-->
-<!--    <Button>-->
-<!--      <icon-location />-->
-<!--      Кнопка-->
-<!--    </Button>-->
+  <div>
+    <header class="header">
+      <div class="container">
+        <div class="header__inner">
+          <span class="header__title">Запомни слово</span>
+          <score-likes :score="100"/>
+        </div>
+      </div>
+    </header>
 
-    <CitySelect @select-city="getCity"/>
-  </main>
+    <main class="main">
+      <div class="container">
+        <div class="main__inner">
+          <div class="main__cards">
+            <card-word v-for="(card, index) in cards"
+                       :key="index"
+                       :is-turn="card.isTurn"
+                       :ru-text="card.ruText"
+                       :en-text="card.enText"
+                       :count="index + 1"
+                       @click-turn="turnCard(card)"
+                       @change-status="console.log($event)"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+
 
 </template>
 
 <style scoped>
-.main {
-  background-color: var(--color-bg-main);
-  padding: 60px 50px;
-  border-radius: 25px;
+.header__inner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 62px 0 66px;
+  height: 121px;
+}
+
+.header__title {
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.12em;
+  text-transform: uppercase;
+
+  color: var(--color-primary-text);
+}
+
+.container {
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.main__inner {
+  padding: 0 62px 0 66px;
+}
+
+.main__cards {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 107px 66px;
 }
 </style>
+
 
 
